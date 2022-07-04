7c6beca90cd998957c653fafd34d05498b12b4c4
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-04 13:06:16 +0530

    Create 3.1.c

---
+++ Exercise/Exercise-3/3.1/3.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void customuniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",-2*log(1-(double)rand()/RAND_MAX));
+}
+fclose(fp);
+
+} 
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+customuniform("custom.dat", 1000000);
+return 0;
+}
