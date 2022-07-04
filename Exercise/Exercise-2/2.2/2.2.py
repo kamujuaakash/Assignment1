1d361dd884a2df2fd9ffc74faa12baa516d28822
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-04 19:00:26 +0530

    Add files via upload

---
+++ Exercise/Exercise-2/2.2/2.2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpmath as mp
+simlen = 1000000
+randvar = np.loadtxt('gau.dat',dtype='double')
+
+
+x = np.linspace(-6,6,40)
+CF=[]
+for i in range(0,40):
+	CF_ind = np.nonzero(randvar < x[i]) 
+	CF_n = np.size(CF_ind)
+	CF.append(CF_n/simlen) 
+	
+	
+def Q(x):
+    return (1-mp.erf(x/mp.sqrt(2)))/2
+    
+def f(x):
+    return 1-Q(x)
+    
+cdf = np.vectorize(f)
+
+plt.plot(x.T,cdf(x))
+plt.plot(x[0:40].T,CF,"o")
+plt.grid()
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
+plt.legend(["Theory","Numerical"])
+
+plt.show()
