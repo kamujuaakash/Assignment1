831e9a0dd97e946f618f9b631c9f5125772168d1
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-04 16:17:07 +0530

    Create 4.1.c

---
+++ Exercise/Exercise-4/4.1/4.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "source.h"
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+sumofuni("tri.dat",1000000);
+return 0;
+}
