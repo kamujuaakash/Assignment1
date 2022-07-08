699780e666886062a9b1b01fd2444125239a6f95
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-08 13:15:08 +0530

    Add files via upload

---
+++ Exercise/Exercise-3/3.1/3.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int main(){
+  FILE* fp;
+  FILE* fp1;
+  double v = 0.0;
+  double u = 0.0;
+  fp = fopen("uni.dat","r");
+  fp1 = fopen("ray.dat","w");
+  while(fscanf(fp,"%lf",&u)!=EOF){
+     v = -2*log(1-u);
+     fprintf(fp1,"%lf\n",v);
+    }
+    fclose(fp);
+    fclose(fp1);
+
+   
+   return 0;
+}
