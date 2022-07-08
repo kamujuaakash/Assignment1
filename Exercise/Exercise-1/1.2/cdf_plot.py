cc00be4f592b6c5cda3b2261928a6f6bd6557c96
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-08 11:10:43 +0530

    Update cdf_plot.py

--- Exercise/Exercise-1/1.2/cdf_plot.py
+++ Exercise/Exercise-1/1.2/cdf_plot.py
@@ -2,10 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# #if using termux
-# import subprocess
-# import shlex
+
 #end if
+def cdf(x):
+    if x<0.0:
+        return 0.0
+    elif x>1.0:
+        return 1.0		
+    else :
+        return x		
+
 
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
@@ -17,31 +23,19 @@ for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
+uni_cdf=scipy.vectorize(cdf)	
 
-#Uniform random variable cdf for a=0 and b=1 in the definition of uniform random variable
-def cdfUniform(x):
-	if(x<=0):
-		x=0
-	elif(x>1):
-		x=1
-	return x
-    
-theoritical = []
-for i in range(len(x)):
-	theoritical.append(cdfUniform(x[i]))
-
-
-plt.plot(x,err,'bo')
-plt.plot(x,theoritical)#plotting the CDF
+	
+plt.plot(x.T,err'bo')#plotting the CDF
+plt.plot(x.T,uni_cdf)
 plt.grid() #creating the grid
-plt.xlabel('$x_i$')
-plt.ylabel('$F_X(x_i)$')
-plt.legend(["Numerical","Theoretical"])
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
 
-# #if using termux
-# plt.savefig('../figs/uni_cdf.pdf')
-# plt.savefig('../figs/uni_cdf.eps')
-# subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
+#if using termux
+#plt.savefig('uni_cdf.pdf')
+#plt.savefig('uni_cdf.eps')
+#subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
 #if using termux
 #plt.savefig('../figs/gauss_cdf.pdf')
 #plt.savefig('../figs/gauss_cdf.eps')
