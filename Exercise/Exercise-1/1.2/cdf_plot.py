0ce7baa1ca3e3384f3e8459cf935b8b088c5fde5
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-08 11:19:58 +0530

    Update cdf_plot.py

--- Exercise/Exercise-1/1.2/cdf_plot.py
+++ Exercise/Exercise-1/1.2/cdf_plot.py
@@ -1,8 +1,15 @@
+
+
 #Importing numpy, scipy, mpmath and pyplot
+
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
 
+#if using termux
+import subprocess
+import shlex
+
+import scipy
 #end if
 def cdf(x):
     if x<0.0:
@@ -10,32 +17,33 @@ def cdf(x):
     elif x>1.0:
         return 1.0		
     else :
-        return x		
+        return x	   
 
 
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-4,4,100)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
+err1 = []
 #randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('uni.dat',dtype='double')
 #randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,30):
-	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-	err_n = np.size(err_ind) #computing the probability
-	err.append(err_n/simlen) #storing the probability values in a list
-uni_cdf=scipy.vectorize(cdf)	
+for i in range(0,100):
+    err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+    err_n = np.size(err_ind) #computing the probability
+    err.append(err_n/simlen) #storing the probability values in a list
 
-	
-plt.plot(x.T,err,'bo')#plotting the CDF
-plt.plot(x.T,uni_cdf)
+uni_cdf = scipy.vectorize(cdf)
+    
+plt.plot(x.T,err,"o")#plotting the CDF
+plt.plot(x.T,uni_cdf(x))
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
-
+plt.legend(["Numerical","Theoritical"])
 #if using termux
-#plt.savefig('uni_cdf.pdf')
-#plt.savefig('uni_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
+plt.savefig('figs/uni_cdf.png')
+# plt.savefig('./figs/uni_cdf.eps')
+# subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
 #if using termux
 #plt.savefig('../figs/gauss_cdf.pdf')
 #plt.savefig('../figs/gauss_cdf.eps')
