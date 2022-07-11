54f0d83ea09549b3e6602692fa67049db2e641d2
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-11 22:02:52 +0530

    Add files via upload

---
+++ Exercise/5.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+
+//Gaussian random numbers
+n_type("ber.dat","gau.dat","5.dat", 1000000);
+
+//Mean of uniform
+//printf("%lf",mean("uni.dat"));
+return 0;
+}
