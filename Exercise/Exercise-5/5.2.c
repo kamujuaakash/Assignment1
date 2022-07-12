6faf5548e74fdcaa42f4ea128a61d6c25b6381be
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-07-12 10:34:49 +0530

    Update 5.2.c

--- Exercise/Exercise-5/5.2.c
+++ Exercise/Exercise-5/5.2.c
@@ -8,7 +8,7 @@ int  main(void) //main function begins
  
 
 //Gaussian random numbers
-n_type("ber.dat","gau.dat","5.dat", 1000000);
+n_type("ber.dat","gau.dat","5.dat",5, 1000000);
 
 //Mean of uniform
 //printf("%lf",mean("uni.dat"));
